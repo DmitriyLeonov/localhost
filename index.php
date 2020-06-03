564076475cdb56128a2a878b55aca3be1c2422dc
DmitriyLeonov <dleonov701@gmail.com>
2020-06-03 17:40:54 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'application/lib/dev.php';
+
+use application\core\Router;
+use application\lib\Db;
+
+spl_autoload_register(function($class){
+    $path = str_replace('\\', '/', $class.'.php'); 
+    if(file_exists($path)){
+        require_once $path;
+    }
+});
+session_start();
+$router = new Router;
+$db = new Db;
+?>
